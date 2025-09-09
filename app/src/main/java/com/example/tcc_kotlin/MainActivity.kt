f28d50c65c6b1fb0792ec4765b8f60aa1ca91c4a
joao_sabino <joaovitorrsabino@gmail.com>
2025-09-09 00:26:03 -0300

    Iniciando menu na MainActivity.kt

--- app/src/main/java/com/example/tcc_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/tcc_kotlin/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.tcc_kotlin.ui.theme.TCC_KotlinTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +29,50 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TCC_KotlinTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column (
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .statusBarsPadding()
+                            .padding(16.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ){
+                        Row(
+                            modifier = Modifier
+                                .fillMaxSize(),
+                            horizontalArrangement = Arrangement.SpaceEvenly,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            OutlinedButton (
+                                onClick = { /*TODO*/ },
+                                modifier = Modifier
+                                    .width(150.dp),
+                                shape = RoundedCornerShape(4.dp)
+                            ) {
+                                Text(
+                                    text = "Autenticação",
+                                    fontSize = 12.sp,
+                                    textAlign = TextAlign.Center
+                                )
+                            };
+                            OutlinedButton (
+                                onClick = { /*TODO*/ },
+                                modifier = Modifier
+                                    .width(150.dp),
+                                shape = RoundedCornerShape(4.dp)
+                            ) {
+                                Text(
+                                    text = "Camera",
+                                    fontSize = 12.sp,
+                                    textAlign = TextAlign.Center)
+                            }
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TCC_KotlinTheme {
-        Greeting("Android")
     }
 }
